12b71ea4bab3e17a94bafe92fbd5c504613f7e49
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-12 07:28:42 +0430

    teaching model added.

---
+++ app/models/teaching.py
@@ -0,0 +1,21 @@
+from app.extensions import db
+
+
+class Teaching(db.Model):
+    __tablename__ = "teachings"
+
+    teacher_id = db.Column(db.String(8), db.ForeignKey("teachers.uid"), nullable=False)
+    subject_id = db.Column(db.String(8), db.ForeignKey("subjects.uid"), nullable=False)
+
+    teacher = db.relationship("Teacher", back_populates="teachings")
+    subject = db.relationship("Subject", back_populates="teachings")
+
+    def to_dict(self) -> dict:
+        return {
+            "teacher_id": self.teacher_id,
+            "subject_id": self.subject_id,
+            **super().to_dict(),
+        }
+
+    def __repr__(self):
+        return f"<Teach Teacher={self.teacher_id} Subject={self.subject_id}>"
