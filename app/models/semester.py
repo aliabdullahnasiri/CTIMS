cf5331343cf491d1c9477adeab8ade1b66c9955e
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-14 08:22:11 +0430

    new model called semester added.

---
+++ app/models/semester.py
@@ -0,0 +1,27 @@
+from app.extensions import db
+
+
+class Semester(db.Model):
+    __tablename__ = "semesters"
+
+    name = db.Column(db.String(50))
+    number = db.Column(db.Integer, nullable=False)
+
+    department_id = db.Column(
+        db.String(8), db.ForeignKey("departments.uid"), nullable=False
+    )
+
+    department = db.relationship("Department", back_populates="semesters")
+    subjects = db.relationship(
+        "Subject", back_populates="semester", cascade="all, delete, delete-orphan"
+    )
+
+    def to_dict(self) -> dict:
+        return {
+            "title": self.title,
+            "number": self.number,
+            **super().to_dict(),
+        }
+
+    def __repr__(self):
+        return f"<Semester {self.name} ID={self.uid}>"
