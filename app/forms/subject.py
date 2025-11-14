fe23ad06eeaeeb93c631f0998741f2f4f25ad1f3
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-14 11:42:43 +0430

    subject form modified.

--- app/forms/subject.py
+++ app/forms/subject.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 from flask_wtf import FlaskForm
@@ -13,8 +14,10 @@ from wtforms import (
 )
 from wtforms.validators import DataRequired, Length, Optional
 
+from app.extensions import db
 from app.models.department import Department
 from app.models.semester import Semester
+from app.models.teacher import Teacher
 
 
 class AddSubjectForm(FlaskForm):
@@ -27,6 +30,8 @@ class AddSubjectForm(FlaskForm):
 
     department_uid = StringField("Department UID", validators=[DataRequired()])
     semester_uid = StringField("Semester UID", validators=[DataRequired()])
+
+    teachers = StringField("Teacher UID", validators=[Optional()])
 
     files = MultipleFileField(
         "Files",
@@ -50,6 +55,25 @@ class AddSubjectForm(FlaskForm):
             raise ValidationError("Not a valid Semester UID.")
         elif not Semester.query.filter_by(uid=semester_uid.data).first():
             raise ValidationError("Semester with the given ID was not found :(")
+
+    def validate_teachers(self, teachers) -> None:
+        pattern: re.Pattern = re.compile(r"^..\d{6}$")
+
+        teachers = json.loads(teachers.data)
+
+        for uid in teachers:
+            if not pattern.search(uid):
+                raise ValidationError(f"Not a valid Teacher UID {uid!r}.")
+
+            if not (
+                db.session.query(Teacher)
+                .filter(
+                    Teacher.uid == uid,
+                )
+                .first()
+            ):
+
+                raise ValidationError("Teacher with the given ID was not found :(")
 
 
 class UpdateSubjectForm(AddSubjectForm):
