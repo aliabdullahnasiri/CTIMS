dea2531fe279c61832e9099f5b4ae7ee28365e3d
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-15 07:53:43 +0430

    class form modified.

--- app/forms/class_.py
+++ app/forms/class_.py
@@ -2,14 +2,16 @@ import re
 
 from flask_wtf import FlaskForm
 from wtforms import HiddenField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length
 
+from app.models.semester import Semester
 from app.models.teacher import Teacher
 
 
 class AddClassForm(FlaskForm):
     name = StringField("Class Name", validators=[DataRequired(), Length(max=255)])
-    teacher_id = StringField("Teacher UID", validators=[Length(8, 8)])
+    teacher_id = StringField("Teacher UID", validators=[DataRequired(), Length(8, 8)])
+    semester_id = StringField("Semester UID", validators=[DataRequired(), Length(8, 8)])
 
     submit = SubmitField("Add Class")
 
@@ -20,6 +22,14 @@ class AddClassForm(FlaskForm):
             raise ValidationError("Not a valid Teacher UID.")
         elif not Teacher.query.filter_by(uid=teacher_id.data).first():
             raise ValidationError("Teacher with the given ID was not found :(")
+
+    def validate_semester_id(self, semester_id) -> None:
+        pattern: re.Pattern = re.compile(r"^..\d{6}$")
+
+        if not pattern.search(semester_id.data):
+            raise ValidationError("Not a valid Semester UID.")
+        elif not Semester.query.filter_by(uid=semester_id.data).first():
+            raise ValidationError("Semester with the given ID was not found :(")
 
 
 class UpdateClassForm(AddClassForm):
