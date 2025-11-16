b67fbcb1c290df4902b52457d1f3bc1abad2987a
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-16 09:41:44 +0430

    admin init file modified.

--- app/blueprints/admin/__init__.py
+++ app/blueprints/admin/__init__.py
@@ -3,12 +3,15 @@ from flask import Blueprint, redirect, url_for
 bp = Blueprint("admin", __name__)
 
 from .routes import (
+    attendance,
     class_,
     dashboard,
     department,
     employee,
+    exam,
     job,
     profile,
+    result,
     semester,
     student,
     subject,
