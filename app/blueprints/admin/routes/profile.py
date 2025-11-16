09ca97dacf5ab90cf89faa8ff542308ba166b4dd
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-11-16 08:27:46 +0430

    new route called profile added.

---
+++ app/blueprints/admin/routes/profile.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from flask_login import current_user, login_required
+
+from app.blueprints.admin import bp
+from app.forms.user import UpdateUserForm
+
+
+@bp.get("/profile")
+@login_required
+def profile():
+    form = UpdateUserForm()
+
+    return render_template(
+        "admin/pages/profile.html",
+        title="Profile",
+        user=current_user,
+        update_user_form=form,
+    )
