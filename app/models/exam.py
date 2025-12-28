fd74f23c4771b1b2aebb26968e9cd44c8a365b97
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2025-12-28 21:26:50 +0430

    the exam model has been modified.

--- app/models/exam.py
+++ app/models/exam.py
@@ -1,6 +1,9 @@
 from typing import Dict
 
+from numerize.numerize import numerize
+
 from app.extensions import db
+from app.models.result import Result
 
 
 class Exam(db.Model):
@@ -13,6 +16,7 @@ class Exam(db.Model):
     exam_time = db.Column(db.Time, nullable=False)
 
     total_marks = db.Column(db.Integer, nullable=False, default=100)
+    min_marks = db.Column(db.Integer, nullable=False, default=50)
 
     subject_id = db.Column(db.String(8), db.ForeignKey("subjects.uid"), nullable=False)
     class_id = db.Column(db.String(8), db.ForeignKey("classes.uid"), nullable=False)
@@ -33,9 +37,30 @@ class Exam(db.Model):
             "exam_date": self.display_exam_date,
             "exam_time": self.display_exam_time,
             "total_marks": self.total_marks,
+            "min_marks": self.min_marks,
             "subject": self.subject.name,
             "class": self.class_.name,
         }
+
+    @property
+    def passed(self):
+        return Result.query.filter(Result.obtained_marks > self.min_marks)
+
+    @property
+    def failed(self):
+        return Result.query.filter(Result.obtained_marks < self.min_marks)
+
+    @property
+    def get_passed(self):
+        return self.passed.all()
+
+    @property
+    def get_failed(self):
+        return self.failed.all()
+
+    @property
+    def min_percentage(self):
+        return round((self.min_marks / self.total_marks) * 100, 2)
 
     @property
     def display_exam_date(self):
@@ -44,3 +69,15 @@ class Exam(db.Model):
     @property
     def display_exam_time(self):
         return self.exam_time.strftime("%H:%M")
+
+    @property
+    def display_number_of_results(self):
+        return numerize(len(self.results), decimals=2)
+
+    @property
+    def display_number_of_passed(self):
+        return numerize(self.passed.count())
+
+    @property
+    def display_number_of_failed(self):
+        return numerize(self.failed.count())
