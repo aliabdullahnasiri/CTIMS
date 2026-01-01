854bf2a7f2f09525fd2dba7809c55b420572096c
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-01 20:09:05 +0430

    the dashboard route has been modified.

--- app/blueprints/admin/routes/dashboard.py
+++ app/blueprints/admin/routes/dashboard.py
@@ -2,10 +2,15 @@ from flask import render_template
 from flask_login import login_required
 
 from app.blueprints.admin import bp
+from app.models.base import Base
 
 
 @bp.get("/")
 @bp.get("/dashboard")
 @login_required
 def dashboard():
-    return render_template("admin/pages/dashboard.html", title="Dashboard", **globals())
+    return render_template(
+        "admin/pages/dashboard.html",
+        title="Dashboard",
+        **{cls.__name__: cls for cls in Base.__subclasses__()}
+    )
