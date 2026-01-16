2a2dea9f3ba716bf0c0fae86bd0f7ed067b1e109
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-16 22:11:41 +0430

    new constant for flask developer added.

--- app/config.py
+++ app/config.py
@@ -29,9 +29,13 @@ class Config:
     APP_DIR = "app"
 
     TD_DIR = f"{APP_DIR}/templates/admin/components/tables/td"
+
     TD_TEMPS = [TEMP for TEMP in pathlib.Path(TD_DIR).glob("*html")]
 
     VIEWS_TEMPS_DIR = f"{APP_DIR}/templates/admin/views"
+
     VIEWS_TEMPS = [TEMP for TEMP in pathlib.Path(VIEWS_TEMPS_DIR).glob("*html")]
 
     UID_PATTERN: re.Pattern = re.compile(r"^..\d{6}$")
+
+    FLASKY_ADMIN: str = "nasiri.aliabdullah@gmail.com"
