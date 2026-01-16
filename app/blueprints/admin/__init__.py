60012ae5ca240eccfe1ec537ad71e0450e48d7a0
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-16 22:06:25 +0430

    the admin route init file file fixied.

--- app/blueprints/admin/__init__.py
+++ app/blueprints/admin/__init__.py
@@ -1,7 +1,16 @@
 from flask import Blueprint
+from flask_login import login_required
 
 from app.functions import __import_all__
+from app.models.user import admin_required
 
 bp = Blueprint("admin", __name__)
+
+
+@bp.before_request
+@login_required
+@admin_required
+def _(): ...
+
 
 __import_all__("app/blueprints/admin/routes")
