2318cde9ed80fd4bfd1541ce7c815565aa7f9e44
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-17 11:31:10 +0430

    access control added!

--- app/models/user.py
+++ app/models/user.py
@@ -1,26 +1,68 @@
 import enum
 from datetime import date
+from functools import wraps
 from typing import Dict, List, Union
 
-from flask import url_for
-from flask_login import UserMixin
+from flask import abort, current_app, url_for
+from flask_login import AnonymousUserMixin, UserMixin, current_user
 
 from app.constants import DEFAULT_AVATAR
 from app.extensions import bcrypt, db, login_manager
 from app.functions import get_file_url
 
 
-@login_manager.user_loader
-def load_user(uid: str):
-    return User.query.filter_by(uid=uid).first()
+@enum.unique
+class PermissionEnum(enum.Enum):
+    CREATE_USER = 0x0000001
+    UPDATE_USER = 0x0000002
+    DELETE_USER = 0x0000004
+    FETCH_USER = 0x0000008
+    FETCH_USERS = 0x0000010
+
+    ADMINISTER = 0x8000000
 
 
-class Role(enum.Enum):
-    ADMIN = "admin"
-    TEACHER = "teacher"
-    STUDENT = "student"
-    EMPLOYEE = "employee"
-    GUEST = "guest"
+class RoleEnum(enum.Enum):
+    ANONYMOUS = 0x0000000, True
+    EMPLOYEE = 0x0000000, False
+    TEACHER = 0x0000000, False
+    STUDENT = 0x0000000, False
+    ADMINISTRATOR = 0xFFFFFFF, False
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+
+    users = db.relationship("User", back_populates="role")
+
+    @staticmethod
+    def insert_roles():
+        try:
+            for r in sorted(
+                [
+                    e
+                    for attr in dir(RoleEnum)
+                    if attr.isupper() and type(e := getattr(RoleEnum, attr)) == RoleEnum
+                ],
+                key=lambda role: role.value.__getitem__(0),
+            ):
+                role = Role.query.filter_by(name=r.name).first()
+
+                if role is None:
+                    role = Role()
+
+                role.name = r.name
+                role.permissions, role.default = r.value
+
+                db.session.add(role)
+                db.session.commit()
+
+        except Exception as err:
+            print(f"ERROR: {err}")
 
 
 class User(UserMixin, db.Model):
@@ -35,15 +77,36 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
 
     birthday = db.Column(db.Date)
-    role = db.Column(db.Enum(Role), default=Role.GUEST, nullable=False)
+    role_uid = db.Column(db.String(8), db.ForeignKey("roles.uid"))
 
-    # Files
     avatar_path = db.Column(
         db.String(255),
         nullable=True,
-    )  # Path to avatar image
+    )
 
     employee = db.relationship("Employee", back_populates="user", cascade="delete")
+    role = db.relationship("Role", back_populates="users")
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        if self.role is None:
+            if self.email == current_app.config["FLASKY_ADMIN"]:
+                self.role = Role.query.filter_by(
+                    permissions=RoleEnum.ADMINISTRATOR.value
+                ).first()
+
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self, permissions):
+        return (
+            self.role is not None
+            and (self.role.permissions & permissions) == permissions
+        )
+
+    def is_administrator(self):
+        return self.can(PermissionEnum.ADMINISTER.value)
 
     def to_dict(self) -> Dict:
         dct = {
@@ -56,7 +119,6 @@ class User(UserMixin, db.Model):
             "birthday": self.display_birthday,
             "age": self.age,
             "avatar": self.avatar_src,
-            "role": self.role.value,
             **super().to_dict(),
         }
 
@@ -112,3 +174,34 @@ class User(UserMixin, db.Model):
                 case "avatar" if type(value) == str:
                     self.avatar_path = get_file_url(value)
                     db.session.commit()
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+@login_manager.user_loader
+def load_user(uid: str):
+    return User.query.filter_by(uid=uid).first()
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not current_user.can(permission):
+                abort(403)
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
+
+
+def admin_required(f):
+    return permission_required(PermissionEnum.ADMINISTER.value)(f)
