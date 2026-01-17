2ff1b64da71e19acf6a047f52abae72e08b2a4b7
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-17 14:06:33 +0430

    the uid column removed.

--- app/models/view.py
+++ app/models/view.py
@@ -5,6 +5,7 @@ class View(db.Model):
     __tablename__ = "views"
 
     id = db.Column(db.Integer, primary_key=True)
+    uid = None
     path = db.Column(db.String(255), nullable=False)
     ip_address = db.Column(db.String(50))
     user_agent = db.Column(db.String(255))
