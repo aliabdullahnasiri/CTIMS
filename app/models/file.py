b67a5cd477986bbf0858f3977e303aabbf3d7937
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-17 20:24:13 +0430

    the file model fixied.

--- app/models/file.py
+++ app/models/file.py
@@ -3,18 +3,15 @@ from datetime import datetime, timezone
 from typing import Optional, Self
 
 import humanize
-from numerize.numerize import numerize
 
 from app.constants import APP_DIR
 from app.extensions import db
 
 
-class FileMixin(object):
-    pass
-
-
 class File(db.Model):
     __tablename__ = "files"
+
+    user_id = db.Column(db.String(8), db.ForeignKey("users.uid"), nullable=False)
 
     # File Info
     file_name = db.Column(
@@ -23,8 +20,10 @@ class File(db.Model):
         default=lambda: datetime.now(timezone.utc).date(),
     )
     file_description = db.Column(db.String(255))
-    file_for = db.Column(db.String(25))
+    file_for = db.Column(db.String(8))
     file_url = db.Column(db.String(255), nullable=False)
+
+    user = db.relationship("User", back_populates="files")
 
     @property
     def path(self: Self) -> pathlib.Path:
@@ -95,48 +94,3 @@ class File(db.Model):
 
     def __repr__(self):
         return f"<File {self.file_name} ({self.human_size}) ID={self.file_id}>"
-
-
-class TeacherFile(db.Model):
-    __tablename__ = "teacher_files"
-
-    teacher_id = db.Column(db.String(8), db.ForeignKey("teachers.uid"), nullable=False)
-    file_id = db.Column(
-        db.String(8), db.ForeignKey("files.uid"), nullable=False, unique=True
-    )
-
-    file = db.relationship("File")
-    teacher = db.relationship("Teacher", back_populates="files")
-
-    def __repr__(self):
-        return f"<TeachertFile ID={self.uid} TeacherID={self.teacher_id} FileID={self.file_id}>"
-
-
-class SubjectFile(db.Model):
-    __tablename__ = "subject_files"
-
-    subject_id = db.Column(db.String(8), db.ForeignKey("subjects.uid"), nullable=False)
-    file_id = db.Column(
-        db.String(8), db.ForeignKey("files.uid"), nullable=False, unique=True
-    )
-
-    file = db.relationship("File")
-    subject = db.relationship("Subject", back_populates="files")
-
-    def __repr__(self):
-        return f"<SubjectFile ID={self.uid} SubjectID={self.subject_id} FileID={self.file_id}>"
-
-
-class StudentFile(db.Model):
-    __tablename__ = "student_files"
-
-    student_id = db.Column(db.String(8), db.ForeignKey("students.uid"), nullable=False)
-    file_id = db.Column(
-        db.String(8), db.ForeignKey("files.uid"), nullable=False, unique=True
-    )
-
-    file = db.relationship("File")
-    student = db.relationship("Student", back_populates="files")
-
-    def __repr__(self):
-        return f"<StudentFile ID={self.uid} StudentID={self.student_id} FileID={self.file_id}>"
