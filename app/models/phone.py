73af74c78b3bb3e3f77692c56e2e73b0d8cee60f
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-17 20:24:28 +0430

    the phone model fixied.

--- app/models/phone.py
+++ app/models/phone.py
@@ -1,62 +1,20 @@
 from app.extensions import db
 
 
-class EmployeePhone(db.Model):
-    __tablename__ = "employee_phones"
+class Phone(db.Model):
+    __tablename__ = "phones"
 
-    employee_id = db.Column(
-        db.String(8), db.ForeignKey("employees.uid"), nullable=False
-    )
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    user_id = db.Column(db.String(8), db.ForeignKey("users.uid"), nullable=False)
+    number = db.Column(db.String(20), unique=True, nullable=False)
 
-    employee = db.relationship("Employee", back_populates="phones")
+    user = db.relationship("User", back_populates="phones")
 
     def to_dict(self) -> dict:
         return {
-            "employee_id": self.uid,
-            "phone_number": self.phone_number,
+            "number": self.number,
+            "user_id": self.user_id,
             **super().to_dict(),
         }
 
     def __repr__(self):
-        return f"<EmployeePhone {self.phone_number} Employee={self.employee_id}>"
-
-
-class TeacherPhone(db.Model):
-    __tablename__ = "teacher_phones"
-
-    teacher_id = db.Column(db.String(8), db.ForeignKey("teachers.uid"), nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
-
-    teacher = db.relationship("Teacher", back_populates="phones")
-
-    def to_dict(self) -> dict:
-        return {
-            "teacher_id": self.teacher_id,
-            "phone_number": self.phone_number,
-            **super().to_dict(),
-        }
-
-    def __repr__(self):
-        return f"<TeacherPhone {self.phone_number} Teacher={self.teacher_id}>"
-
-
-class StudentPhone(db.Model):
-    __tablename__ = "student_phones"
-
-    student_id = db.Column(db.String(8), db.ForeignKey("students.uid"), nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
-
-    student = db.relationship("Student", back_populates="phones")
-
-    def to_dict(self) -> dict:
-        """Return a dictionary representation of the StudentPhone."""
-        return {
-            "phone_id": self.uid,
-            "student_id": self.student_id,
-            "phone_number": self.phone_number,
-            **super().to_dict(),
-        }
-
-    def __repr__(self):
-        return f"<StudentPhone {self.phone_number} Student={self.student_id}>"
+        return f"<Phone {self.number} Employee={self.user_id}>"
