a66ae5a36723b106810b84edca7cb96985cefe09
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-23 22:42:03 +0430

    the model fixied.

--- app/models/job.py
+++ app/models/job.py
@@ -1,8 +1,13 @@
 from decimal import Decimal
+from operator import call
 from typing import Dict
+
+from numerize.numerize import numerize
+from sqlalchemy import func
 
 from app.constants import CURRENCY_SYMBOL
 from app.extensions import db
+from app.models.employee import Employee
 
 
 class Job(db.Model):
@@ -14,46 +19,53 @@ class Job(db.Model):
     max_salary = db.Column(db.Numeric(12, 2), nullable=False)
 
     employees = db.relationship(
-        "Employee", back_populates="job", cascade="all, delete, delete-orphan"
+        "Employee",
+        back_populates="job",
+        cascade="all, delete, delete-orphan",
+        lazy="dynamic",
     )
 
     def __repr__(self):
-        return f"<Job {self.job_title} ID={self.uid}>"
+        return f"<Job title={self.job_title!r}>"
 
     def to_dict(self) -> Dict:
         dct = {
-            "job_id": self.uid,
             "job_title": self.job_title,
             "job_description": self.job_description,
             "min_salary": float(self.min_salary) if self.min_salary else None,
             "max_salary": float(self.max_salary) if self.max_salary else None,
-            **super().to_dict(),
+            **call(getattr(super(), "to_dict")),
         }
 
         return dct
 
     @property
     def employee_count(self) -> int:
-        return len(self.employees)
+        return self.employees.count()
 
     @property
     def average_salary(self) -> float | None:
-        if not self.employees:
-            return None
-        total_salary = sum([float(emp.salary or 0) for emp in self.employees])
-        return total_salary / len(self.employees)
+        return (
+            db.session.query(func.avg(Employee.salary).label("a"))
+            .filter(Employee.job_uid == getattr(self, "uid"))
+            .scalar()
+        )
 
     @property
     def highest_salary(self) -> float | None:
-        if not self.employees:
-            return None
-        return max([float(emp.salary or 0) for emp in self.employees])
+        return (
+            db.session.query(func.max(Employee.salary).label("a"))
+            .filter(Employee.job_uid == getattr(self, "uid"))
+            .scalar()
+        )
 
     @property
     def lowest_salary(self) -> float | None:
-        if not self.employees:
-            return None
-        return min([float(emp.salary or 0) for emp in self.employees])
+        return (
+            db.session.query(func.min(Employee.salary).label("a"))
+            .filter(Employee.job_uid == getattr(self, "uid"))
+            .scalar()
+        )
 
     @property
     def display_min_salary(self) -> str:
@@ -107,4 +119,4 @@ class Job(db.Model):
 
     @property
     def display_employee_count(self) -> str:
-        return str(self.employee_count)
+        return numerize(self.employee_count)
