3f319cae3a24411cbd192ed932691586d67b01b0
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-23 22:41:02 +0430

    the model fixied.

--- app/models/attendance.py
+++ app/models/attendance.py
@@ -1,5 +1,6 @@
 import enum
 from datetime import datetime, timezone
+from operator import call
 
 from app.extensions import db
 
@@ -23,12 +24,11 @@ class StudentAttendance(db.Model):
 
     def to_dict(self):
         return {
-            "uid": self.uid,
             "date": self.display_date,
             "time": self.display_time,
             "status": self.status.value,
             "student": self.student.to_dict(),
-            **super().to_dict(),
+            **call(getattr(super(), "to_dict")),
         }
 
     @property
@@ -40,7 +40,7 @@ class StudentAttendance(db.Model):
         return self.time.strftime("%H:%M:%S") if self.time else "N/A"
 
     def __repr__(self):
-        return f"<StudentAttendance {self.uid} StudentID={self.student_id}>"
+        return f"<StudentAttendance StudentID={self.student_id}>"
 
 
 class TeacherAttendance(db.Model):
@@ -57,15 +57,15 @@ class TeacherAttendance(db.Model):
     teacher = db.relationship("Teacher", back_populates="attendances")
 
     def __repr__(self):
-        return f"<TeacherAttendance {self.uid} TeacherID={self.teacher_id}>"
+        return f"<TeacherAttendance TeacherID={self.teacher_id}>"
 
     def to_dict(self):
         return {
-            "uid": self.uid,
             "date": self.display_date,
             "time": self.display_time,
             "status": self.status.value,
             "teacher": self.teacher.to_dict(),
+            **call(getattr(super(), "to_dict")),
         }
 
     @property
