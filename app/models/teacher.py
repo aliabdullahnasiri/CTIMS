05edef501026e269b8441e10546f1044a467199b
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-01-23 22:42:34 +0430

    the model fixied.

--- app/models/teacher.py
+++ app/models/teacher.py
@@ -1,11 +1,9 @@
+from operator import call
 from typing import List
-
-from numerize.numerize import numerize
 
 from app.constants import CURRENCY_SYMBOL
 from app.extensions import db
-from app.models.file import File
-from app.models.teaching import Teaching
+from app.models.subject import Subject
 
 
 class Teacher(db.Model):
@@ -15,31 +13,23 @@ class Teacher(db.Model):
     user_uid = db.Column(
         db.String(8), db.ForeignKey("users.uid"), nullable=False, unique=True
     )
-
     salary = db.Column(db.Numeric(12, 2), nullable=True)
 
     time = db.relationship("Time", back_populates="teachers")
     user = db.relationship("User", cascade="delete")
 
-    teachings = db.relationship(
-        "Teaching", back_populates="teacher", cascade="all, delete, delete-orphan"
-    )
     attendances = db.relationship(
         "TeacherAttendance",
         back_populates="teacher",
         cascade="all, delete, delete-orphan",
+        lazy="dynamic",
     )
     classes = db.relationship(
-        "Class", back_populates="teacher", cascade="all, delete, delete-orphan"
+        "Class",
+        back_populates="teacher",
+        cascade="all, delete, delete-orphan",
+        lazy="dynamic",
     )
-
-    @property
-    def files(self):
-        return [file for file in File.query.filter_by(file_for=self.user.uid).all()]
-
-    @property
-    def subjects(self):
-        return [teaching.subject for teaching in self.teachings]
 
     @property
     def display_salary(self) -> str:
@@ -58,17 +48,9 @@ class Teacher(db.Model):
         avg_salary = sum(salaries) / len(salaries)
         return float(self.salary) > avg_salary
 
-    @property
-    def display_number_of_classes(self):
-        return numerize(len(self.classes), decimals=2)
-
-    @property
-    def display_number_of_teachings(self):
-        return numerize(len(self.teachings), decimals=2)
-
     def to_dict(self):
         dct = {
-            "teacher_id": self.uid,
+            "teacher_id": getattr(self, "uid"),
             "user_uid": self.user.uid,
             "time_id": self.time_id,
             "full_name": self.user.full_name,
@@ -82,34 +64,28 @@ class Teacher(db.Model):
             "salary": f"{self.salary:.2f}" if self.salary is not None else None,
             "display_salary": self.display_salary,
             "avatar": self.user.avatar_path,
-            "phones": [p.number for p in self.user.phones],
-            "subjects": [s.subject.uid for s in self.teachings],
-            "files": [f.to_dict() for f in self.files],
-            **super().to_dict(),
+            "files": [f.to_dict() for f in self.user.files.all()],
+            "phones": [p.number for p in self.user.phones.all()],
+            "subjects": [s.uid for s in getattr(self, "subjects").all()],
+            **call(getattr(super(), "to_dict")),
         }
 
         return dct
 
     def update_subjects(self, subjects: List[str]):
-        for teaching in self.teachings:
-            if teaching.subject_id not in subjects:
-                db.session.delete(teaching)
+        __subjects__ = getattr(self, "subjects")
 
-        db.session.commit()
+        print(list(__subjects__.all()))
+        for subject in __subjects__.all():
+            if subject.uid not in subjects:
+                __subjects__.remove(subject)
 
         for subject in subjects:
-            if Teaching.query.filter_by(
-                subject_id=subject, teacher_id=self.uid
-            ).first():
+            if __subjects__.filter_by(uid=subject).scalar():
                 continue
 
-            teaching = Teaching()
-            teaching.teacher_id = self.uid
-            teaching.subject_id = subject
-
-            db.session.add(teaching)
-
-        db.session.commit()
+            subject = Subject.query.filter_by(uid=subject).scalar()
+            __subjects__.append(subject)
 
     def __repr__(self):
-        return f"<Teacher {self.user.full_name} ID={self.uid}>"
+        return f"<Teacher full_name={self.user.full_name!r}>"
