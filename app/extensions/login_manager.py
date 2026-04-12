77a97f90e4fbf36354acea5ef2172446f40d2b56
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-04-12 10:05:45 +0430

    Modified.

--- app/extensions/login_manager.py
+++ app/extensions/login_manager.py
@@ -1,3 +1,6 @@
 from flask_login import LoginManager
 
 login_manager = LoginManager()
+
+setattr(login_manager, "login_view", "admin.sign_in")
+setattr(login_manager, "login_message_category", "info")
