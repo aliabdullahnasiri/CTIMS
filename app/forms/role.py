eaced84803b832f11e4b85d96ed41bba167270fe
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-05-29 09:59:48 +0430

    ~

--- app/forms/role.py
+++ app/forms/role.py
@@ -24,13 +24,5 @@ class AddRoleForm(Form):
 
 class UpdateRoleForm(AddRoleForm):
     uid = HiddenField("Role UID", validators=[DataRequired()])
-    name = StringField("Name", validators=[Length(max=255)])
-
-    description = TextAreaField(
-        "Description", validators=[Optional(), Length(max=2500)]
-    )
-
-    default = BooleanField("Default", default=False, validators=[Optional()])
-    permissions = StringField("Permissions", validators=[Optional()])
 
     submit = SubmitField("Update Role")
