2777b15dede6d4dbd8c3918f0784e0d537d0e332
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-05-29 08:00:52 +0430

    ~

--- app/blueprints/admin/routes/profile.py
+++ app/blueprints/admin/routes/profile.py
@@ -2,16 +2,14 @@ from flask import render_template
 from flask_login import current_user
 
 from app.blueprints.admin import bp
-from app.forms.user import UpdateUserForm
+from app.forms.profile import UpdateProfileForm
 
 
 @bp.get("/profile")
 def profile():
-    form = UpdateUserForm()
-
     return render_template(
         "admin/pages/profile.html",
         title="Profile",
         user=current_user,
-        update_user_form=form,
+        update_user_form=UpdateProfileForm(),
     )
