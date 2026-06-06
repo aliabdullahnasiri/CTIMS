9de3483cb85b6cef74a760879494d25a0beac0f0
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-06-06 20:35:51 +0430

    ~

--- app/forms/student.py
+++ app/forms/student.py
@@ -5,10 +5,10 @@ from app.forms.user import AddUserForm, UpdateUserForm
 
 
 class AddStudentForm(AddUserForm):
-    class_id = StringField("Class UID", validators=[DataRequired(), Length(8, 8)])
     submit = SubmitField("Add")
 
 
 class UpdateStudentForm(UpdateUserForm, AddStudentForm):
     uid = HiddenField("Student UID", validators=[DataRequired()])
+    class_id = StringField("Class UID", validators=[Length(8, 8)])
     submit = SubmitField("Update Student")
