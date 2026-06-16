ca0550c9ee7dc18e404c4d03788c9b3347d9e0c3
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-06-16 18:41:43 +0430

    the form has been completed.

--- app/forms/daily_section.py
+++ app/forms/daily_section.py
@@ -1,7 +1,45 @@
-from app.forms import Form
+from flask_babel import gettext as _
+from wtforms import HiddenField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+
+from app.forms import Form, MustBeUnique, ValidateUID
+from app.models.daily_section import DailySection
+from app.models.exam import Exam
 
 
-class AddDailySectionForm(Form): ...
+class AddDailySectionForm(Form):
+    exam_uid = StringField(
+        _("EXAM_UID_LABEL"),
+        validators=[
+            DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")),
+            Length(max=8, message=_("THIS_FIELD_CANNOT_EXCEED_8_CHARACTERS_MSG")),
+            ValidateUID(Exam),
+            MustBeUnique(Exam, "uid"),
+        ],
+    )
+
+    title = StringField(
+        _("TITLE_LABEL"),
+        validators=[
+            DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")),
+            Length(max=255, message=_("THIS_FIELD_CANNOT_EXCEED_255_CHARACTERS_MSG")),
+        ],
+    )
+
+    description = TextAreaField(
+        _("DESCRIPTION_LABEL"),
+    )
+
+    submit = SubmitField(_("ADD_LABEL"))
 
 
-class UpdateDailySectionForm(AddDailySectionForm): ...
+class UpdateDailySectionForm(AddDailySectionForm):
+    uid = HiddenField(
+        _("DAILY_SECTION_UID_LABEL"),
+        validators=[
+            DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")),
+            ValidateUID(DailySection),
+        ],
+    )
+
+    submit = SubmitField(_("UPDATE_LABEL"))
