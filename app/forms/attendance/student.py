184b7dc4b12a65e13ec2cbfff3d41c4cec48c20d
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-06-16 21:00:39 +0430

    ~

--- app/forms/attendance/student.py
+++ app/forms/attendance/student.py
@@ -1,29 +1,34 @@
-import re
-
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Length
 from flask_babel import gettext as _
+from wtforms import SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
+from app.forms import Form, ValidateUID
 from app.models.attendance import AttendanceStatus
 from app.models.student import Student
 
 
-class AddStudentAttendanceForm(FlaskForm):
-    student_id = StringField(_("STUDENT_UID_LABEL"), validators=[DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")), Length(min=8, max=8, message=_("THIS_FIELD_MUST_BE_8_CHARACTERS_MSG"))])
+class AddStudentAttendanceForm(Form):
+    student_id = StringField(
+        _("STUDENT_UID_LABEL"),
+        validators=[
+            DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")),
+            Length(min=8, max=8, message=_("THIS_FIELD_MUST_BE_8_CHARACTERS_MSG")),
+            ValidateUID(Student),
+        ],
+        render_kw={
+            "data-auto-complete": "true",
+            "data-fetch-api": "api.autocomplete",
+            "data-model-name": "Student",
+            "data-select-val": "uid",
+            "data-search-col": "uid",
+            "data-template": "students.html",
+        },
+    )
     status = SelectField(
         _("STATUS_LABEL"),
         validators=[DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR"))],
         choices=[AttendanceStatus.ABSENT.value, AttendanceStatus.PRESENT.value],
         default=AttendanceStatus.PRESENT.value,
     )
-
-    def validate_student_id(self, student_id) -> None:
-        pattern: re.Pattern = re.compile(r"^S.\d{6}$")
-
-        if not pattern.search(student_id.data):
-            raise ValidationError(_("NOT_A_VALID_STUDENT_UID_MSG"))
-        elif not Student.query.filter_by(uid=student_id.data).first():
-            raise ValidationError(_("STUDENT_WITH_THE_GIVEN_ID_WAS_NOT_FOUND_MSG"))
 
     submit = SubmitField(_("ADD_ATTENDANCE_LABEL"))
--- app/forms/attendance/teacher.py
+++ app/forms/attendance/teacher.py
@@ -1,29 +1,34 @@
-import re
-
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField, ValidationError
-from wtforms.validators import DataRequired, Length
 from flask_babel import gettext as _
+from wtforms import SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
+from app.forms import Form, ValidateUID
 from app.models.attendance import AttendanceStatus
 from app.models.teacher import Teacher
 
 
-class AddTeacherAttendanceForm(FlaskForm):
-    teacher_id = StringField(_("TEACHER_UID_LABEL"), validators=[DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")), Length(min=8, max=8, message=_("THIS_FIELD_MUST_BE_8_CHARACTERS_MSG"))])
+class AddTeacherAttendanceForm(Form):
+    teacher_id = StringField(
+        _("TEACHER_UID_LABEL"),
+        validators=[
+            DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR")),
+            Length(min=8, max=8, message=_("THIS_FIELD_MUST_BE_8_CHARACTERS_MSG")),
+            ValidateUID(Teacher),
+        ],
+        render_kw={
+            "data-auto-complete": "true",
+            "data-fetch-api": "api.autocomplete",
+            "data-model-name": "Teacher",
+            "data-select-val": "uid",
+            "data-search-col": "uid",
+            "data-template": "teachers.html",
+        },
+    )
     status = SelectField(
         _("STATUS_LABEL"),
         validators=[DataRequired(message=_("THIS_FIELD_IS_REQUIRED_ERROR"))],
         choices=[AttendanceStatus.ABSENT.value, AttendanceStatus.PRESENT.value],
         default=AttendanceStatus.PRESENT.value,
     )
-
-    def validate_teacher_id(self, teacher_id) -> None:
-        pattern: re.Pattern = re.compile(r"^T.\d{6}$")
-
-        if not pattern.search(teacher_id.data):
-            raise ValidationError(_("NOT_A_VALID_TEACHER_UID_MSG"))
-        elif not Teacher.query.filter_by(uid=teacher_id.data).first():
-            raise ValidationError(_("TEACHER_WITH_THE_GIVEN_ID_WAS_NOT_FOUND_MSG"))
 
     submit = SubmitField(_("ADD_ATTENDANCE_LABEL"))
