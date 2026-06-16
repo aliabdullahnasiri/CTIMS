bf7c19ae1e5ec010ff3720ae8ef3857717583c75
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-06-16 17:55:42 +0430

    ~

---
+++ app/models/daily_section.py
@@ -0,0 +1,41 @@
+from operator import call
+
+from app.extensions.db import db
+
+
+class DailySection(db.Model):
+    __tablename__ = "daily_section"
+
+    exam_uid = db.Column(
+        db.String(8),
+        db.ForeignKey("exams.uid"),
+        nullable=True,
+        index=True,
+    )
+
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+
+    date = db.Column(db.Date, nullable=False)
+
+    exam = db.relationship(
+        "Exam",
+        back_populates="daily_sections",
+    )
+
+    students = db.relationship(
+        "Student",
+        back_populates="daily_section",
+        cascade="all, delete-orphan",
+        lazy="dynamic",
+    )
+
+    def to_dict(self) -> Dict:
+        return {
+            "exam_uid": self.exam_uid,
+            "title": self.title,
+            "description": self.description,
+            "date": self.date.strftime("%Y-%m-%d") if self.date else None,
+            "students": [student.to_dict() for student in self.students.all()],
+            **call(getattr(super(), "to_dict")),
+        }
