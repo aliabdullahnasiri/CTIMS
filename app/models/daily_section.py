938459a260bfcbe47f1d72a367dbf3c2c0e81789
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2026-06-25 11:21:03 +0430

    ~

--- app/models/daily_section.py
+++ app/models/daily_section.py
@@ -1,7 +1,11 @@
 from operator import call
 from typing import Dict
 
+from sqlalchemy import and_
+
 from app.extensions.db import db
+from app.models.exam import Exam
+from app.models.result import Result
 
 
 class DailySection(db.Model):
@@ -31,6 +35,38 @@ class DailySection(db.Model):
         cascade="all, delete-orphan",
         lazy="dynamic",
     )
+
+    @property
+    def passed_students(self):
+        return (
+            result.student
+            for result in Result.query.join(
+                Exam, Result.exam_id == getattr(Exam, "uid")
+            )
+            .filter(
+                and_(
+                    Result.exam_id == self.exam_uid,
+                    (Result.obtained_marks * 100.0 / Exam.total_marks)
+                    >= Exam.min_percentage,
+                )
+            )
+            .all()
+        )
+
+    @property
+    def get_next_base_number(self):
+        Student = db.Model.registry._class_registry.get("Student")
+
+        student = (
+            Student.query.filter(Student.base_number > 0)
+            .order_by(Student.base_number.desc())
+            .first()
+        )
+
+        if student:
+            return student.base_number + 1
+
+        return self.starting_base_number
 
     def to_dict(self) -> Dict:
         return {
